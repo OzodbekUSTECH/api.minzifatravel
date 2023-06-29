1465d6ca35c2e7052758d2ad3609d26029e93785
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-06-30 01:06:13 +0500

    3rd try to save voice m essage

--- tg.py
+++ tg.py
@@ -29,7 +29,7 @@ api_hash = 'c3ca5ae4e368b18eccd06a5edcd7eec0'
 tgclient = Client("minzifaapi", api_id=api_id, api_hash=api_hash)
 
 db = Session()
-@tgclient.on_message(filters.private & filters.incoming) 
+@tgclient.on_message(filters.private & filters.incoming)
 async def handle_private_message(client: Client, message: TelegramMessage):
     # Проверка, является ли отправитель пользователем
     if message.from_user:
@@ -37,9 +37,8 @@ async def handle_private_message(client: Client, message: TelegramMessage):
         first_name = message.from_user.first_name or ""
         last_name = message.from_user.last_name or ""
         message_text = message.text or ""
-        
+
         language = detect_user_language(message_text)
-        
 
         lead = db.query(Lead).filter_by(chat_id=lead_id).first()
 
@@ -70,7 +69,7 @@ async def handle_private_message(client: Client, message: TelegramMessage):
                     await client.send_message(lead.chat_id, "Добро пожаловать в MINZIFA TRAVEL!")
                 else:
                     await client.send_message(lead.chat_id, "Welcome to MINZIFA TRAVEL!")
-  
+
             elif better_manager:
                 better_manager.has_additional_client = True
                 lead.manager_id = better_manager.id
@@ -84,7 +83,7 @@ async def handle_private_message(client: Client, message: TelegramMessage):
                     await client.send_message(lead.chat_id, "<b><em>Добро пожаловать в MINZIFA TRAVEL!</em></b>\n\nИзвините, в данный момент все менеджеры заняты. Пожалуйста, ожидайте.")
                 else:
                     await client.send_message(lead.chat_id, "<b><em>Welcome to MINZIFA TRAVEL!</em></b>\n\nSorry, all managers are busy at the moment. Please wait.")
-                
+
                 content = None
                 filename = None
                 filepath = None
@@ -109,39 +108,45 @@ async def handle_private_message(client: Client, message: TelegramMessage):
                     filepath = f"crm-ut.com/static/files/{filename}"
                     content = message.caption or None
                 elif message.voice:
-                # Генерация уникального имени файла
+                    # Генерация уникального имени файла
                     filename = f"voice_{message.voice.file_unique_id}.ogg"
                     file_path = os.path.join("/home/api.minzifatravel/static/files", filename)  # Полный путь для сохранения голосового сообщения
                     await message.download(file_path)
                     filepath = f"crm-ut.com/static/files/{filename}"
                     content = message.caption or None
+                    # Создание объекта файла и связь с сообщением, пользователем, менеджером и чатрумом
+                    file = File(
+                        filename=filename,
+                        filepath=filepath,
+                        lead=lead,
+                        manager=None
+                    )
+                    db.add(file)
+                    db.commit()
+
+                    user_message = Message(
+                        text=codecs.encode(content, 'utf-8'),
+                        lead=lead,
+                        manager=None,
+                        file_id=file.id
+                    )
+
+                    db.add(user_message)
+                    db.commit()
+
                 elif message.text:
                     content = message.text
 
-                user_message = Message(
-                    text=codecs.encode(content, 'utf-8'),
-                    lead = lead,
-                    manager = None
-                )
+                    user_message = Message(
+                        text=codecs.encode(content, 'utf-8'),
+                        lead=lead,
+                        manager=None
+                    )
 
-                if filename:
-                        # Создание объекта файла и связь с сообщением, пользователем, менеджером и чатрумом
-                        file = File(
-                            filename=filename,
-                            filepath=filepath,
-                            lead=lead,
-                            manager=None
-                        )
-                        db.add(file)
-                        db.commit()
-                        
-                        user_message.file_id = file.id
-
-                db.add(user_message)
-                db.commit()
+                    db.add(user_message)
+                    db.commit()
 
                 while True:
-                    
                     await asyncio.sleep(5)
                     available_manager = db.query(User).filter_by(role="Отдел продаж", language=lead.language, is_busy=False).first()
                     if available_manager:
@@ -149,7 +154,7 @@ async def handle_private_message(client: Client, message: TelegramMessage):
                         lead.manager_id = available_manager.id
 
                         for message in lead.messages:
-                             message.manager = available_manager
+                            message.manager = available_manager
 
                         for file in lead.files:
                             file.manager = available_manager
@@ -158,10 +163,8 @@ async def handle_private_message(client: Client, message: TelegramMessage):
                         db.commit()
                         await client.send_message(lead.chat_id, f"Добро пожаловать в MINZIFA TRAVEL!")
                         break
-                
-            
 
-        #если уже есть
+        # Если уже есть
         content = None
         filename = None
         filepath = None
@@ -175,46 +178,53 @@ async def handle_private_message(client: Client, message: TelegramMessage):
         elif message.photo:
             # Генерация уникального имени файла
             filename = f"photo_{message.photo.file_unique_id}.jpg"
-            file_path = os.path.join("/home/api.minzifatravel/static/files", filename) # Полный путь для сохранения фото
+            file_path = os.path.join("/home/api.minzifatravel/static/files", filename)  # Полный путь для сохранения фото
             await message.download(file_path)
             filepath = f"crm-ut.com/static/files/{filename}"
             content = message.caption or None
         elif message.video:
             filename = f"video_{message.video.file_unique_id}.mp4"  # Use .mp4 extension for video files
-            file_path = os.path.join("/home/api.minzifatravel/static/files", filename) # Полный путь для сохранения видео
+            file_path = os.path.join("/home/api.minzifatravel/static/files", filename)  # Полный путь для сохранения видео
             await message.download(file_path)
             filepath = f"crm-ut.com/static/files/{filename}"
             content = message.caption or None
         elif message.voice:
-        # Генерация уникального имени файла
+            # Генерация уникального имени файла
             filename = f"voice_{message.voice.file_unique_id}.ogg"
             file_path = os.path.join("/home/api.minzifatravel/static/files", filename)  # Полный путь для сохранения голосового сообщения
             await message.download(file_path)
             filepath = f"crm-ut.com/static/files/{filename}"
             content = message.caption or None
+            # Создание объекта файла и связь с сообщением, пользователем, менеджером и чатрумом
+            file = File(
+                filename=filename,
+                filepath=filepath,
+                lead=lead,
+                manager=lead.manager
+            )
+            db.add(file)
+            db.commit()
+
+            user_message = Message(
+                text=codecs.encode(content, 'utf-8'),
+                lead=lead,
+                manager=lead.manager,
+                file_id=file.id
+            )
+
+            db.add(user_message)
+            db.commit()
+
         elif message.text:
             content = message.text
 
-        user_message = Message(
-            text=codecs.encode(content, 'utf-8'),
-            lead = lead,
-            manager = lead.manager
-        )
+            user_message = Message(
+                text=codecs.encode(content, 'utf-8'),
+                lead=lead,
+                manager=lead.manager
+            )
 
-        if filename:
-                # Создание объекта файла и связь с сообщением, пользователем, менеджером и чатрумом
-                file = File(
-                    filename=filename,
-                    filepath=filepath,
-                    lead=lead,
-                    manager=lead.manager
-                )
-                db.add(file)
-                db.commit()
-                
-                user_message.file_id = file.id
+            db.add(user_message)
+            db.commit()
 
-        db.add(user_message)
-        db.commit()
-
-tgclient.run()
+    tgclient.run()
